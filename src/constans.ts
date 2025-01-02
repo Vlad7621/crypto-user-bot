eeefa543c1d18a17d0256e7909ff104904637738
SovereignCountry <bohdanfedasdev@gmail.com>
2025-01-02 16:42:06 +0200

    fix

--- src/constans.ts
+++ src/constans.ts
@@ -2,6 +2,6 @@ export const NEWS_BOT_ID = 7362664019;
 export const COIN_CHANNEL_ID = -1001124574831;
 export const CHANNEL_ID = -1002349778856;
 
-// export const NEWS_BOT_ID = -4725499397;
-// export const COIN_CHANNEL_ID = -4725499397;
+// export const NEWS_BOT_ID = 6696286956;
+// export const COIN_CHANNEL_ID = -1002284135312;
 // export const CHANNEL_ID = -1002167526471;
