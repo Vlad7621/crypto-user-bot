8aa5ff3a1c904c6cc35079ba24db85328bfef6f7
andriyilkiv <andriyilkiv322@gmail.com>
2025-01-03 10:33:32 +0200

    change id

--- src/constans.ts
+++ src/constans.ts
@@ -1,7 +1,7 @@
-// export const NEWS_BOT_ID = 7362664019;
-// export const COIN_CHANNEL_ID = -1001124574831;
-// export const CHANNEL_ID = -1002349778856;
-
 export const NEWS_BOT_ID = 7362664019;
-export const COIN_CHANNEL_ID = -1002284135312;
-export const CHANNEL_ID = -1002167526471;
+export const COIN_CHANNEL_ID = -1001124574831;
+export const CHANNEL_ID = -1002349778856;
+
+// export const NEWS_BOT_ID = 7362664019;
+// export const COIN_CHANNEL_ID = -1002284135312;
+// export const CHANNEL_ID = -1002167526471;
