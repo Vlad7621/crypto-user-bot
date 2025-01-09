e9687540d661d86fcd79f7156271d73a82d44da1
SovereignCountry <bohdanfedasdev@gmail.com>
2025-01-09 16:01:34 +0200

    validation

--- src/helpers/validation.ts
+++ src/helpers/validation.ts
@@ -30,7 +30,7 @@ export const validationEta = (
       const hours = match[1] ? parseInt(match[1]) : 0;
       const minutes = match[2] ? parseInt(match[2]) : 0;
 
-      if (hours > 0 || minutes > 4) {
+      if (hours > 0 || minutes >= 4) {
          return true;
       }
    }
