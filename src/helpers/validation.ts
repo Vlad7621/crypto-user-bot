ec2975ec414b5b453d4fbab9ede6dcc71a66a4f9
SovereignCountry <bohdanfedasdev@gmail.com>
2025-01-10 11:36:48 +0200

    filter

--- src/helpers/validation.ts
+++ src/helpers/validation.ts
@@ -9,7 +9,7 @@ export const validationFrequency = (
       const number = parseFloat(match[1].replace(/,/g, ''));
       const suffix = match[2] || '';
 
-      if (number > 5 && !!suffix) {
+      if (number > 4 && !!suffix) {
          return true
       }
 
